02229bdf259038b71b8dc48d725967132a755fe6
Dorien Brown <77749428+deedot718@users.noreply.github.com>
2021-03-05 02:14:32 -0500

    This is my survey project.

---
+++ nba_survey.py
@@ -0,0 +1,78 @@
+answer = input ('Do you want to take my NBA survey?')
+if answer == "Yes" or answer == "yes":
+    print('Cool here we go')
+shooter = input ('Better shooter Curry or Bird?')
+if shooter == "Curry" or shooter == "curry":
+    print('Curry has made a lot of threes')
+if shooter == "Bird" or shooter == "bird":
+    print('NBA 3pt Shooting Champion')
+else:
+    print("Do you know ball or what?? ")
+
+dunker = input ('Better dunker Carter or Jordan?')
+if dunker == "Carter" or dunker == "carter":
+    print("Carter dunked over players")
+if dunker == "Jordan" or dunker == "jordan":
+    print('Slam Dunk Champion')
+else:
+    print("Do you know ball or what?? ")
+
+crossover = input ('Better crossover Iverson or Kyrie?')
+if crossover == "Iverson" or crossover == "iverson":
+    print('Iverson had a killer crossover')
+if crossover == "Kyrie" or crossover == "kyrie":
+    print('Kyrie got the ball on a string')
+else:
+    print("Do you know ball or what?? ")
+
+    team = input ('Best Lakers or Celtics?')
+    if team == "Lakers" or team == "lakers":
+        print("Lakers have won the most NBA Championships")
+    if team == "Celtics" or team == "celtics":
+        print('Celtics once won eight Championships in a row')
+    else:
+        print("Do you know ball or what?? ")
+
+uniform = input ('Best NBA uniforms Nuggets or Jazz?')
+if uniform == "Nuggets" or uniform == "nuggets":
+    print('Something about that logo')
+if uniform == "Jazz" or uniform == "jazz":
+    print('Purple is for royalty')
+else:
+    print("Do you know ball or what?? ")
+
+fans = input ('Best NBA fans Knicks or Nets?')
+if fans == "Knicks" or fans == "knicks":
+    print('Something about being at MSG')
+if fans == "Nets" or fans == "nets":
+    print('Brooklyn people do not know how to act')
+else:
+    print("Do you know ball or what?? ")
+
+center = input ('Better center Kareem or Shaq?')
+if center == "Kareem" or center == "kareem":
+    print('New York Finest')
+if center == "Shaq" or center == "Shaq":
+    print('New Jersey Finest')
+else:
+    print("Do you know ball or what?? ")
+
+
+sg = input ('Better shooting guard Kobe or Wade?')
+if sg == "Kobe" or sg == "kobe":
+    print('Kobe one of the greatest')
+if sg == "Wade" or sg == "wade":
+    print('Wade future hall of famer')
+else:
+    print("Do you know ball or what?? ")
+print('Thanks for taking my survey')
+
+
+
+
+
+
+
+
+
+
